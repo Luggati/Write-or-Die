828f51d56bbd05279c92c006dfe26b97445679fb
Malorbian <pi-j@web.de>
2024-03-05 21:26:07 +0100

    Deathscreen added + Renames

--- Assets/Skripts/DeathScreenScript.cs
+++ Assets/Skripts/DeathScreenScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UiScript : MonoBehaviour
+public class DeathScreenScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
